d43959ef796e5dafba18f00babdb727dad6fa3e3
Andresouzanl <devals2022@gmail.com>
2024-10-02 04:02:37 -0300

    atualizando0210

--- src/app/(pagina)/page.tsx
+++ src/app/(pagina)/page.tsx
@@ -3,12 +3,12 @@ import ProdutoItem from "@/components/produto/ProdutoItem";
 import useProdutos from "@/data/hooks/useProdutos";
 
 export default function Inicio() {
-  const produtos = useProdutos();
+  const { produtos } = useProdutos();
   return (
     <div className="grid grid-cols-4 gap-5 container">
-      {produtos.map((produto) => (
-        <ProdutoItem key={produto.id} produto={produto} />
-      ))}
+       {produtos.map((produto) => (
+                <ProdutoItem key={produto.id} produto={produto} />
+            ))}
     </div>
   );
 }
--- src/app/produto/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import useProdutos from "@/data/hooks/useProdutos"
-
-export default function PaginaProduto(props: any){
-  const id = +props.params.id
-  const  produtos  = useProdutos()
-  const produto = produtos.find(p => p.id === id)
-  return produto ? (<div>{produto?.nome}</div>) : <div>Produto não encontrado</div>
-}
